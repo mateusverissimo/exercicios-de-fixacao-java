52cde046ae162e04e52c4f181f635c1a6ec66302
Mateus Verissimo <mateusverissimocoelho@gmail.com>
2025-04-23 10:51:36 -0300

    Revert "inicio a lambdas e interface"
    This reverts commit f2e57bf3a724d32fdd0227f87df691a5b30d380d.

--- aulas/src/lambdas/Calculo.java
+++ aulas/src/lambdas/Calculo.java
@@ -4,12 +4,5 @@ package lambdas;
 public interface Calculo {
 
     public abstract double executar(double a, double b);
-
-    default String legal() {
-        return "legal";
-    }
-
-    static String muitoLegal() {
-        return "muito legal";
-    }
+    double teste();
 }
